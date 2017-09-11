4b408c7b09c181ab87d3fd249f44a37b3624f504
Kohei Mizuki <silversoul.wt@gmail.com>
2017-09-11 16:31:28 +0900

    dummy は受け取らない

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ import (
 )
 
 //{"tick":{"instrument":"USD_JPY","time":"2017-09-08T20:59:58.315562Z","bid":107.832,"ask":107.858}}
+//{"heartbeat":{"time":"2017-09-11T07:12:35.258498Z"}}
+
+type Dummy struct {
+	HeartBeart struct {
+		Time time.Time `json:"time"`
+	} `json:"heartbeat""`
+}
 
 type Change struct {
 	Tick struct {
@@ -37,18 +44,28 @@ func main() {
 	client := new(http.Client)
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Cannot get resp: %v", err)
+		log.Printf("Cannot get resp: %v", err)
 	}
 	defer resp.Body.Close()
 
 	reader := bufio.NewReader(resp.Body)
 	var c Change
+	var d Dummy
+	var prevtime time.Time
+	var count int
 	for {
 		line, _ := reader.ReadBytes('\n')
-		err := json.Unmarshal(line, &c)
+		err := json.Unmarshal(line, &d)
 		if err != nil {
-			log.Println("Cannot decode json: ", err)
+			log.Printf("Cannot decode json to Dummy: %v", err)
 		}
-		fmt.Println(c)
+		if prevtime == d.HeartBeart.Time {
+			err = json.Unmarshal(line, &c)
+			if err != nil {
+				log.Printf("Cannot decode json to Change: %v", err)
+			}
+			fmt.Println(c)
+		}
+		prevtime = d.HeartBeart.Time
 	}
 }
